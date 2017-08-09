500b4627ae1c90477f9dbe272775677d895a27fd
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-08-09 15:22:01 +0300

    Add console app in attempt to reproduce the issue

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using NSubstitute;
+
+namespace ConsoleApp1
+{
+    public class Foo
+    {
+        internal virtual async Task<(bool Succeeded, string Message)> DoStuff(string str)
+        {
+            await Task.Delay(10);
+            return (true, "hello");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sub = Substitute.ForPartsOf<Foo>();
+            sub.WhenForAnyArgs(async x =>
+            {
+                try
+                {
+                    await x.DoStuff(Arg.Any<string>());
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }).DoNotCallBase();
+
+        }
+    }
+}
